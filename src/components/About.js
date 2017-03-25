e2ea39bbd5595f4a3ad575f659dc4f798a96f05e
vitormalencar <vitor.maia.alencar@gmail.com>
2017-03-25 14:17:29 -0300

    add about component

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import {h} from 'preact';
+
+const About = () => (
+	<div class="main__container-about">
+		<div class="main__tag">About Me</div>
+		<p class="main__copy">FrontEnd Developer, focused on new and scalable
+			technologies, graduating on information systems and looking for new
+			opportunities where I can develop my skills and be part of amazing
+			projects.</p>
+	</div>
+);
+
+export default About;
