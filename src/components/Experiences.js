babcc4b44bd05b23680989dbd9d9955495e35351
vitormalencar <vitor.maia.alencar@gmail.com>
2017-03-25 14:18:14 -0300

    add experience component

---
+++ src/components/Experiences.js
@@ -0,0 +1,41 @@
+import {h} from 'preact';
+
+const Experiences = () => (
+	<div class="main__container-experiences">
+		<div class="main__tag">Experience</div>
+		<div class="main__experience--first">
+			<h2 class="main__experience-title">
+				<a href="https://www.agendakidsdigital.com">AGENDAKIDS</a>
+			</h2>
+			<p class="main__experience-description">My role in Agendakids is to build
+				products related to communication for schools. I work with scalable
+				applications, building the company website and hybrid mobile
+				application. My main focus is FrontEnd development always thinking on
+				the best performance.</p>
+		</div>
+		<div class="main__experience">
+			<h2 class="main__experience-title">
+				<a href="https://github.com/deviofoundation">DEVIO Foundation</a>
+			</h2>
+			<p class="main__experience-description">The main idea of DEVIO Foundation
+				is to unite all development groups in one place, making it possible to
+				contribute actively to the growth of local community development. The
+				main idea is to share free knowledge and connect people, enabling to
+				envolve the community as a whole.</p>
+		</div>
+		<div class="main__experience">
+			<h2 class="main__experience-title">
+				<a href="http://gdgfortaleza.org">GDG Fortaleza</a>
+			</h2>
+			<p class="main__experience-description">Google Developer Groups (GDG) are
+				for developers who are interested in Google's developer technology;
+				Everything from Android, Chrome, Drive, and Google Cloud platforms, to
+				product APIs like the Cast API, Maps API, and YouTube API. The program
+				also has the important role of educating the community, helping them
+				to get involve with technology and improve their knowledge, making
+				network and having great opportunities.</p>
+		</div>
+	</div>
+);
+
+export default Experiences;
