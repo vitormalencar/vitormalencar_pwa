54d7edabb815d1b383901bcca9d8bcfd7bc71858
vitormalencar <vitor.maia.alencar@gmail.com>
2017-03-25 14:20:12 -0300

    add basic footer social component]

---
+++ src/components/InfoFooter.js
@@ -0,0 +1,30 @@
+import {h} from 'preact';
+
+const InfoFooter = () => (
+	<div class="info-footer">
+		<div class="info-footer__social">
+			<a
+				href="https://twitter.com/vitormalencar"
+				target="_blank"
+				rel="noopener"
+				class="fa fa-twitter info-footer__social-icon--twitter"></a>
+			<a
+				href="https://github.com/vitormalencar"
+				target="_blank"
+				rel="noopener"
+				class="fa fa-github info-footer__social-icon--github"></a>
+			<a
+				href="https://www.instagram.com/vitormalencar"
+				target="_blank"
+				rel="noopener"
+				class="fa fa-instagram info-footer__social-icon--instagram"></a>
+			<a
+				href="https://www.linkedin.com/in/vitormalencar"
+				target="_blank"
+				rel="noopener"
+				class="fa fa-linkedin info-footer__social-icon--linkedin"></a>
+		</div>
+	</div>
+);
+
+export default InfoFooter;
