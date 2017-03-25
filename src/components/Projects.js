13a228d219e149109b5918209e5b1871d0b1354c
vitormalencar <vitor.maia.alencar@gmail.com>
2017-03-25 14:19:44 -0300

    add projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,22 @@
+import {h} from 'preact';
+
+const Projects = () => (
+	<div class="main__container-projects">
+		<div class="main__tag">Projects</div>
+		<div class="main__project">
+			<div class="main__project-info">
+				<div class="main__project-info-company">MILTECNOLOGIA</div>
+				<div class="main__project-info-position">Developer</div>
+			</div>
+			<div class="main__project-year">2014 - 2015</div>
+			<div class="main__project"></div>
+			<div class="main__project-info">
+				<div class="main__project-info-company">Banco do Nordeste</div>
+				<div class="main__project-info-position">Developer</div>
+			</div>
+			<div class="main__project-year">2013 - 2014</div>
+		</div>
+	</div>
+);
+
+export default Projects;
