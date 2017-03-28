14e2cac3215647a578673047c0a0f703e945af98
vitormalencar <vitor.maia.alencar@gmail.com>
2017-03-27 21:49:19 -0300

    Add about container

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import {h} from 'preact';
+import Style from './style.scss';
+
+const About = ({about}) => (
+	<div class="main__container-about">
+		<div class="main__tag">About Me</div>
+		<p class="main__headline">{about}
+			<a href='https://speakerdeck.com/vitormalencar/getting-started-with-progressive-web-apps'>progressive web apps</a>,
+			<a href='https://slides.com/robertovitormaia/deck-4-13-15/fullscreen'>Funcional programming</a>
+			and <a href='https://speakerdeck.com/vitormalencar/ionic-rocks'>hybrid apps.</a></p>
+	</div>
+);
+
+export default About;
