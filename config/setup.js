d487b04dc17589c854f6aa2d252058d45930d3d1
vitormalencar <vitor.maia.alencar@gmail.com>
2017-04-02 18:52:08 -0300

    update webpack setup

--- config/setup.js
+++ config/setup.js
@@ -1,5 +1,5 @@
 const glob = require('glob');
-const { join } = require('path');
+const {join} = require('path');
 const webpack = require('webpack');
 const Copy = require('copy-webpack-plugin');
 const HTML = require('html-webpack-plugin');
@@ -11,7 +11,9 @@ const ExtractText = require('extract-text-webpack-plugin');
 const CnameWebpackPlugin = require('cname-webpack-plugin');
 const CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const CompressionPlugin = require("compression-webpack-plugin");
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 const uglify = require('./uglify');
 const babel = require('./babel');
@@ -21,19 +23,26 @@ const root = join(__dirname, '..');
 module.exports = isProd => {
 	// base plugins array
 	const plugins = [
-		new Clean(['dist'], { root }),
-		new Copy([{ context: 'src/static/', from: '**/*.*' }]),
-		new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' }),
+		new Clean(['dist'], {root}),
+		new Copy([{context: 'src/static/',from: '**/*.*'}]),
+		new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
 		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development')
+			'process.env.NODE_ENV': JSON.stringify(isProd
+				? 'production'
+				: 'development')
 		}),
-		new HTML({ template: 'src/index.html' }),
+		new HTML({
+			minify:{
+				collapseWhitespace:true
+			},
+			template: 'src/index.html',
+			inlineSource: '.(js)$'
+		}),
+		new HtmlWebpackInlineSourcePlugin(),
 		new webpack.LoaderOptionsPlugin({
 			options: {
 				babel,
-				postcss: [
-					require('autoprefixer')({ browsers: ['last 3 version'] })
-				]
+				postcss: [require('autoprefixer')({browsers: ['last 3 version']})]
 			}
 		})
 	];
@@ -42,36 +51,24 @@ module.exports = isProd => {
 		babel.presets.push('babili');
 
 		plugins.push(
-			new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
+			new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
 			new webpack.optimize.UglifyJsPlugin(uglify),
 			new ExtractText('styles.[hash].css'),
+			new StyleExtHtmlWebpackPlugin(),
 			new PurifyCSSPlugin({
 				paths: glob.sync(join(__dirname, 'src/*.html')),
-				moduleExtensions:['.html','.js'],
+				moduleExtensions: ['.html', '.js'],
 				verbose: true,
 				minimize: true
-			}),
-			new CriticalPlugin({
-		    src: 'index.html',
-		    inline: true,
-		    minify: true,
-		    dest: 'index.html'
-  	}),
-			new SWPrecache({
-				minify: true,
-				filename: 'service-worker.js',
-				dontCacheBustUrlsMatching: /./,
-				navigateFallback: 'index.html',
-				staticFileGlobsIgnorePatterns: [/\.map$/],
-				runtimeCaching: [{
-          handler: 'cacheFirst',
-					urlPattern: /\.googleapis\.com\//,
-        }]
-			}),
-			new CnameWebpackPlugin({
-					 domain: 'www.vitormalencar.com'
-			 })
-		);
+		}),
+		new SWPrecache({
+			minify: true,
+			filename: 'service-worker.js',
+			dontCacheBustUrlsMatching: /./,
+			navigateFallback: 'index.html',
+			staticFileGlobsIgnorePatterns: [/\.map$/]
+		}),
+		new CnameWebpackPlugin({domain: 'www.vitormalencar.com'}));
 	} else {
 		// dev only
 		plugins.push(
