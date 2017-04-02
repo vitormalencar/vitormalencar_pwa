89d8f8a2e90edca9a33a95947ce4fae80ad57462
vitormalencar <vitor.maia.alencar@gmail.com>
2017-04-02 18:55:44 -0300

    mount component direct on index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import { render } from 'preact';
-import './index.sass';
+import {h, render} from 'preact';
 
-let elem, App;
-function init() {
-	App = require('./views').default;
-	elem = render(App, document.getElementById('root'), elem);
+import './index.sass';
+import data from './data.json';
+import SideNav from './components/SideNav';
+import InfoMain from './components/InfoMain';
+import InfoFooter from './components/InfoFooter';
+import MainContent from './components/MainContent';
+
+let elem;
+
+const App = () => (
+	<div id='page'>
+		<SideNav>
+			<InfoMain
+				name={data.name}
+				role={data.role}
+				mail={data.mail}/>
+			<InfoFooter/>
+		</SideNav>
+		<MainContent {...data}/>
+	</div>
+);
+
+const init = () => {
+	elem = render(<App/>, document.getElementById('root'), elem);
 }
 
 init();
@@ -14,12 +33,19 @@ if (process.env.NODE_ENV === 'production') {
 	if ('serviceWorker' in navigator && location.protocol === 'https:') {
 		navigator.serviceWorker.register('/service-worker.js');
 	}
-
 	// add Google Analytics
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	(function(i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function() {
+			(i[r].q = i[r].q || []).push(arguments)
+		},
+		i[r].l = 1 * new Date();
+		a = s.createElement(o),
+		m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 	ga('create', 'UA-89577115-1', 'auto');
 	ga('send', 'pageview');
 } else {
