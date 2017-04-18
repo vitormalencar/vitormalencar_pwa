4ed0b1c4953d7badf91afabe87ebb5fb5758df76
vitormalencar <vitor.maia.alencar@gmail.com>
2017-04-18 01:04:34 -0300

    use more than one type of picture

--- src/components/InfoMain/index.js
+++ src/components/InfoMain/index.js
@@ -6,11 +6,12 @@ import HeadShot from '../HeadShot';
 import InfoFooter from '../InfoFooter';
 import ElevatorText from '../ElevatorText';
 
-import avatar from '../../static/avatar/vitor.webp';
+import webp from '../../static/avatar/vitor.webp';
+import png from '../../static/avatar/vitor.png';
 
 const InfoMain = ({name, role, mail}) => (
 	<div class="info-main">
-		<HeadShot avatar={avatar} name={name}/>
+		<HeadShot webp={webp} png={png} name={name}/>
 		<ElevatorText name={name} role={role}/>
 		<Button mail={mail}/>
 	</div>
