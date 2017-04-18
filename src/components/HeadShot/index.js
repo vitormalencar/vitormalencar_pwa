d45a4ff0a0f1dd9d74b9c9ac670f9e22fa595034
vitormalencar <vitor.maia.alencar@gmail.com>
2017-04-18 01:04:57 -0300

    update img to picture element

--- src/components/HeadShot/index.js
+++ src/components/HeadShot/index.js
@@ -1,9 +1,13 @@
 import {h} from 'preact';
 import Style from './style.scss';
 
-const HeadShot = ({avatar, name}) => (
+const HeadShot = ({webp, png, name}) => (
 	<div class='info__headshot'>
-		<img src={avatar} alt={name} class="info__headshot_img"/>
+		<picture class="info__headshot_img">
+			<source srcset={webp} type="image/webp"/>
+			<source srcset={png} type="image/png"/>
+			<img src={png} alt={name} class="info__headshot_img"/>
+		</picture>
 	</div>
 );
 
