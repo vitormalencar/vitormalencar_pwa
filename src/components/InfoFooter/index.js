bed62d8801fb3f07c359a508d3f2b9014bfd06da
vitormalencar <vitor.maia.alencar@gmail.com>
2017-04-23 17:40:52 -0300

    update icon components

--- src/components/InfoFooter/index.js
+++ src/components/InfoFooter/index.js
@@ -6,33 +6,41 @@ const InfoFooter = () => (
 	<div className="info-footer">
 		<div className="info-footer__social">
 			<a
+				href="skype:vitormalencar?userinfo"
+				target="_blank"
+				rel="noopener"
+				className="info-footer__social-icon--skype">
+				<Icon icon='SKYPE' color='white' size='22'/>
+			</a>
+			<a
 				href="https://twitter.com/vitormalencar"
 				target="_blank"
 				rel="noopener"
-				className="fa fa-twitter info-footer__social-icon--twitter">
+				className="info-footer__social-icon--twitter">
 				<Icon icon='TWITTER' color='white' size='22'/>
-				</a>
+			</a>
 			<a
 				href="https://github.com/vitormalencar"
 				target="_blank"
 				rel="noopener"
-				className="fa fa-github info-footer__social-icon--github">
-					<Icon icon='GITHUB' color='white' size='22'/>
-				</a>
+				className="info-footer__social-icon--github">
+				<Icon icon='GITHUB' color='white' size='22'/>
+			</a>
 			<a
 				href="https://www.instagram.com/vitormalencar"
 				target="_blank"
 				rel="noopener"
-				className="fa fa-instagram info-footer__social-icon--instagram">
-					<Icon icon='INSTAGRAM' color='white' size='22'/>
-				</a>
+				className="info-footer__social-icon--instagram">
+				<Icon icon='INSTAGRAM' color='white' size='22'/>
+			</a>
 			<a
 				href="https://www.linkedin.com/in/vitormalencar"
 				target="_blank"
 				rel="noopener"
-				className="fa fa-linkedin info-footer__social-icon--linkedin">
-					<Icon icon='LINKEDIN2' color='white' size='22'/>
-				</a>
+				className="info-footer__social-icon--linkedin">
+				<Icon icon='LINKEDIN' color='white' size='22'/>
+			</a>
+
 		</div>
 	</div>
 );
