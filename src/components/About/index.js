00d932249f000e5bb1f71495909a554394b27c5d
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-07 16:40:19 -0300

    update About Component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,12 +5,16 @@ const About = ({about}) => (
 	<div className="main__container-about">
 		<div className="main__tag">About Me</div>
 		<p className="main__headline">
-			Experienced and ambitious web developer and speaker, focused on new and scalable technologies,
-			who loves sharing knowledge and cares about self-improvement.
-			Enjoys talking in events and meetings about:
-			<a href='https://speakerdeck.com/vitormalencar/getting-started-with-progressive-web-apps'>progressive web apps ⚛</a>,
-			<a href='https://slides.com/robertovitormaia/deck-4-13-15/fullscreen'>funcional programming ƛ</a>
-			and <a href='https://speakerdeck.com/vitormalencar/ionic-rocks'>hybrid development. ⚡️</a></p>
+			Experienced and ambitious web developer and speaker, focused on new
+			and scalable technologies, who loves sharing knowledge and cares about
+			self-improvement. Enjoys talking in events and meetings about:
+			<br/>
+			<ul class='talks'>
+			<li><a href='https://speakerdeck.com/vitormalencar/getting-started-with-progressive-web-apps'>⚛ Progressive web apps</a></li>
+			<li><a href='https://slides.com/robertovitormaia/deck-4-13-15/fullscreen'>ƛ Funcional programming</a></li>
+			<li><a href='https://speakerdeck.com/vitormalencar/ionic-rocks'>⚡️ Hybrid development.</a></li>
+			</ul>
+		</p>
 	</div>
 );
 
