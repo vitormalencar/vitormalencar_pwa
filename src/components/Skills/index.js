b57215c815f7dd2478f1a23e5ce189369aa6a872
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-07 16:41:10 -0300

    add Skills Component

---
+++ src/components/Skills/index.js
@@ -0,0 +1,25 @@
+import {h} from 'preact';
+import Style from './style.scss';
+const Skills = () => (
+	<div class="main__container-about">
+		<div class="main__tag">Technical</div>
+		<p class="main__headline"> I currently have a lot of fun working with the following technologies:
+		<br/>
+		<br/>
+		<ul class="main__list">
+			<li class="main__list-item">ES6</li>
+			<li class="main__list-item">React</li>
+			<li class="main__list-item">Redux</li>
+			<li class="main__list-item">Express</li>
+			<li class="main__list-item">Angular</li>
+			<li class="main__list-item">Sass</li>
+			<li class="main__list-item">Gulp</li>
+			<li class="main__list-item">Ionic</li>
+			<li class="main__list-item">Webpack</li>
+			<li class="main__list-item">MongoDB</li>
+		</ul>
+		</p>
+	</div>
+);
+
+export default Skills;
