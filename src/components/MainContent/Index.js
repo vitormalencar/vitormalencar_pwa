b14655afc587e971bfa2b757a26b83f4dd2a5999
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-07 16:41:01 -0300

    add Skills component to main

--- src/components/MainContent/Index.js
+++ src/components/MainContent/Index.js
@@ -5,11 +5,13 @@ import Resume from '../Resume';
 import Style from './style.scss';
 import Projects from '../Projects';
 import Achivements from '../Achivements';
+import Skills from '../Skills';
 import Experiences from '../Experiences';
 
 const MainContent = ({projects, experiences, about,resume}) => (
 	<div className="container-main">
 		<About about={about}/>
+		<Skills/>
 		<Experiences experiences={experiences}/>
 		<Projects projects={projects}/>
 		<Resume resume={resume}/>
