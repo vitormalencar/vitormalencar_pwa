916f661eb25f4aa45e8d208e5efe6011cfeb39dc
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-27 17:07:30 -0300

    update dependencies

--- config/webpack.js
+++ config/webpack.js
@@ -1,4 +1,4 @@
-const {join} = require('path');
+const { join } = require('path');
 const ExtractText = require('extract-text-webpack-plugin');
 const setup = require('./setup');
 
@@ -6,50 +6,51 @@ const dist = join(__dirname, '../dist');
 const exclude = /(node_modules|bower_components)/;
 
 module.exports = env => {
-	const isProd = env && env.production;
+  const isProd = env && env.production;
 
-	return {
-		entry: {
-			app: './src/index.js',
-			vendor: ['preact']
-		},
-		output: {
-			path: dist,
-			filename: '[name].[hash].js',
-			publicPath: '/'
-		},
-		resolve: {
-			alias: {
-				'react': 'preact/aliases',
-				'react-dom': 'preact/aliases'
-			}
-		},
-		module: {
-			rules: [
-				{
-					test: /\.jsx?$/,
-					exclude: exclude,
-					loader: 'babel-loader'
-				}, {
-					test: /\.(ttf|eot|svg|png|jpg|webp)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-					loader: "file-loader"
-				}, {
-					test: /\.(sass|scss)$/,
-					loader: isProd
-						? ExtractText.extract({fallbackLoader: 'style-loader', loader: 'css-loader!postcss-loader!sass-loader'})
-						: 'style-loader!css-loader!postcss-loader!sass-loader'
-				}
-			]
-		},
-		plugins: setup(isProd),
-		devtool: !isProd && 'eval',
-		devServer: {
-			contentBase: dist,
-			port: process.env.PORT || 3000,
-			historyApiFallback: true,
-			compress: isProd,
-			inline: !isProd,
-			hot: !isProd
-		}
-	};
+  return {
+    entry: {
+      app: './src/index.js',
+      vendor: ['preact']
+    },
+    output: {
+      path: dist,
+      filename: '[name].[hash].js',
+      publicPath: '/'
+    },
+    resolve: {
+      alias: {
+        'react': 'preact/aliases',
+        'react-dom': 'preact/aliases'
+      }
+    },
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        exclude: exclude,
+        loader: 'babel-loader'
+      }, {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        include: /src/
+      }, {
+        test: /\.(ttf|eot|svg|png|jpg|webp)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }, {
+        test: /\.(sass|scss)$/,
+        loader: isProd ? ExtractText.extract({ fallbackLoader: 'style-loader', loader: 'css-loader!postcss-loader!sass-loader' }) : 'style-loader!css-loader!postcss-loader!sass-loader'
+      }]
+    },
+    plugins: setup(isProd),
+    devtool: !isProd && 'eval',
+    devServer: {
+      contentBase: dist,
+      port: process.env.PORT || 3000,
+      historyApiFallback: true,
+      compress: isProd,
+      inline: !isProd,
+      hot: !isProd
+    }
+  };
 };
